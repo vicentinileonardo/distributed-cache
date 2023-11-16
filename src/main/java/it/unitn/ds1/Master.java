b9dcc239785a8f5d979e0ad31a4834bfa6d6113b
Jclock <jacopo.clocchiatti@gmail.com>
2023-11-17 00:43:32 +0100

    Fix some issue regarding access to variables, modified inconsistency print

--- src/main/java/it/unitn/ds1/Master.java
+++ src/main/java/it/unitn/ds1/Master.java
@@ -11,14 +11,56 @@ import java.util.Map;
 public class Master extends AbstractActor {
 
 	ActorRef database;
+
+	private ActorRef getDatabase() {
+		return this.database;
+	}
+
 	HashSet<ActorRef> l1Caches;
+
+	private HashSet<ActorRef> getL1Caches() {
+		return this.l1Caches;
+	}
+
 	HashSet<ActorRef> l2Caches;
+
+	private HashSet<ActorRef> getL2Caches() {
+		return this.l2Caches;
+	}
 
 	HashMap<Integer, Integer> data;
 
+	private void addData(Map<Integer, Integer> newData) {
+		this.data.putAll(newData);
+	}
+
+	private HashMap<Integer, Integer> getData(){
+		return this.data;
+	}
+
 	HashSet<ActorRef> requestsSent;
 
+	private void addRequest(ActorRef actor){
+		this.requestsSent.add(actor);
+	}
+
+	private void removeRequest(ActorRef actor){
+		this.requestsSent.remove(actor);
+	}
+
+	private boolean isRequestsEmpty(){
+		return this.requestsSent.isEmpty();
+	}
+
 	boolean isConsistent = true;
+
+	private boolean getConsistentStatus() {
+		return this.isConsistent;
+	}
+
+	private void setInconsistentStatus(){
+		this.isConsistent = false;
+	}
 
 	public Master(ActorRef db, HashSet<ActorRef> l1, HashSet<ActorRef> l2) {
 		this.database = db;
@@ -43,38 +85,43 @@ public class Master extends AbstractActor {
 
 	private void onStartHealthCheck(Message.StartHealthCheck msg){
 		Message.HealthCheckRequestMsg new_msg = new Message.HealthCheckRequestMsg();
-		this.database.tell(new_msg, getSelf());
+		getDatabase().tell(new_msg, getSelf());
 	}
 
 	private void onHealthCheckResponse(Message.HealthCheckResponseMsg msg){
 		if (getSender() == database){
-			this.data.putAll(msg.getData());
+			addData(msg.getData());
 
 			Message.HealthCheckRequestMsg new_msg = new Message.HealthCheckRequestMsg();
-			for (ActorRef cache : l1Caches) {
+			for (ActorRef cache : getL1Caches()) {
 				cache.tell(new_msg, self());
-				this.requestsSent.add(cache);
+				addRequest(cache);
 			}
 
-			for (ActorRef cache : l2Caches) {
+			for (ActorRef cache : getL2Caches()) {
 				cache.tell(new_msg, self());
-				this.requestsSent.add(cache);
+				addRequest(cache);
 			}
 
 		} else {
+			boolean currentConsistency = true;
 			for (Map.Entry<Integer, Integer> entry : msg.getData().entrySet()) {
-				if (entry.getValue() != this.data.get(entry.getKey())) {
-					this.isConsistent = false;
-					System.out.println("Actor "+getSender().toString()+" is inconsistent!");
-					break;
+				if (entry.getValue() != getData().get(entry.getKey())) {
+					setInconsistentStatus();
+					if (currentConsistency){
+						System.out.println("Actor "+getSender().toString()+" is inconsistent!");
+						currentConsistency = false;
+					}
+					System.out.println("Inconsistent value at key: " + entry.getKey() +
+							" -> Actor value: " + entry.getValue() + "|| DB value: " + getData().get(entry.getKey()));
 				}
 			}
 
-			this.requestsSent.remove(getSender());
+			removeRequest(getSender());
 		}
 
-		if (this.requestsSent.isEmpty()) {
-			System.out.println("Is the system consistent? "+ this.isConsistent);
+		if (isRequestsEmpty()) {
+			System.out.println("Is the system consistent? "+ getConsistentStatus());
 		}
 	}
 
